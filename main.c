03c61aace108e58d837d5f9e034ca05fc216c75d
Felipe Prado <jfpc11@misena.edu.co>
2021-05-12 22:44:07 -0500

    the code working as spected but valgrind has some issues context erros but not memory leaks

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
-#include "header.h"
+#include "monty.h"
+
 /**
 * main - entry point.
 * @ac: arguments counter.
 * @av: arguments vector.
 * Return: Always 0.
 */
+
+
 int main(int ac, char *av[])
 {
-	int i = 0, lines = 1;
+	int i = 0;
+	unsigned int lines = 1;
 	FILE *fd;
 	char buf[1024], **tokens = NULL, *delimit = " \t\n\r\a", *token = NULL;
 	stack_t *head = NULL;
@@ -27,6 +31,11 @@ int main(int ac, char *av[])
 	{
 		i = 0;
 		tokens = malloc(sizeof(char *) * strlen(buf));
+		if (tokens == NULL)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
 		token = strtok(buf, delimit);
 		while (token)
 		{
@@ -36,28 +45,12 @@ int main(int ac, char *av[])
 		}
 		tokens[i] = NULL;
 		global_data = tokens[1];
-		i = 0;
-		while(tokens[i] != NULL)
-		{
-			/**printf("%s\n", tokens[i]);*/
-			i++;
-		}
-		string_cmp(tokens, lines, &head);
+		if (tokens[0] != NULL)
+			string_cmp(tokens, lines, &head);
 		lines++;
 		free_tokenizer(tokens);
 	}
-    free_stack(head);
+	free_stack(head);
 	fclose(fd);
-	return (0);
-}
-
-void free_tokenizer(char **free_token)
-{
-	int i = 0;
-	while(free_token[i])
-	{
-		free(free_token[i]);
-		i++;
-	}
-	free(free_token);
+	return (EXIT_SUCCESS);
 }
